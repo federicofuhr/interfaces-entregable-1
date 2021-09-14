124bc6336922850e980629530c014ed481840e87
juanbarthes <jbarthes@alumnos.exa.unicen.edu.ar>
2021-09-13 22:38:53 -0300

    Añadido el control del HSL
    Añadidas las funcionalidades para poder controlar los valores de matiz, saturación y brillo como filtros separados y combinados.

--- js/main.js
+++ js/main.js
@@ -41,6 +41,12 @@ function init() {
     // Boton del filtro blur
     let btnBlur = document.querySelector("#blur-filter");
 
+    // Boton para activar los controles de matiz, saturacion y brillo (HSL)
+    let btnHSL = document.querySelector("#hsl-filter");
+
+    // Boton para aplicar los cambios en el HSL
+    let btnApplyHSL = document.querySelector("#hsl-apply");
+
     // mouseX y mouseY almacenan la posicion del cursor en X e Y, respecto al padding del elemento currentTarget (para lapiz y goma)
     let mouseX = 0;
     let mouseY = 0;
@@ -76,7 +82,7 @@ function init() {
 
     // Evento para el filtro de desenfoque
     btnBlur.addEventListener("click", function () {
-        ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
+        //ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
         imageData = ctx.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
         roamImage(imageData, applyBlurFilter);
         ctx.putImageData(imageData, 0, 0);
@@ -84,7 +90,7 @@ function init() {
 
     // Evento para el filtro de binarizacion
     btnGrayscale.addEventListener("click", function () {
-        ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
+        //ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
         imageData = ctx.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
         roamImage(imageData, applyGrayFilter);
         ctx.putImageData(imageData, 0, 0);
@@ -92,16 +98,31 @@ function init() {
 
     // Evento para el filtro negativo
     btnNegative.addEventListener("click", function () {
-        ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
+        //ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
         imageData = ctx.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
         roamImage(imageData, applyNegativeFilter);
         ctx.putImageData(imageData, 0, 0);
     });
 
+    // Evento para el filtro sepia
     btnSepia.addEventListener("click", function () {
-        ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
+        //ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
         imageData = ctx.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
         roamImage(imageData, applySepiaFilter);
+        ctx.putImageData(imageData, 0, 0);
+    });
+
+    //Evento para mostrar los controles de matiz saturacion y brilo
+    btnHSL.addEventListener("click", () => {
+        document.querySelector("#hsv-controls").classList.toggle("collapse");
+    })
+
+    //Evento para aplicar los cambios al HSL
+    btnApplyHSL.addEventListener('click', () => {
+        // Creo la imagen para aplicar el filtro de hsl
+        //ctx.drawImage(originalImage, 0, 0, imageScaledWidth, imageScaledHeight);
+        imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        roamImage(imageData, adjustHSL);
         ctx.putImageData(imageData, 0, 0);
     });
 
@@ -278,6 +299,160 @@ function init() {
         data[i] = 255 - data[i];
         data[i + 1] = 255 - data[i + 1];
         data[i + 2] = 255 - data[i + 2];
+    }
+
+    function setPixel(imageData, index, rgb) {
+        let data = imageData.data;
+        data[index] = rgb.r;
+        data[index + 1] = rgb.g;
+        data[index + 2] = rgb.b;
+    }
+    
+    function rgbToHsl(r, g, b) {
+        // Dividir los componentes rgb por 255.
+        r /= 255;
+        g /= 255;
+        b /= 255;
+    
+        // Encontrar el maximo y el minimo entre los componentes rgb y calcular la resta.
+        let cmin = Math.min(r, g, b);
+        let cmax = Math.max(r, g, b);
+        let delta = cmax - cmin;
+        // Declarar e inicializar los componentes hsl.
+        let h = 0;
+        let s = 0;
+        let l = 0;
+    
+        // Calcular la matiz (hue).
+        // r, g y b son iguales.
+        if (delta == 0)
+            h = 0;
+        // r es el maximo.
+        else if (cmax == r)
+            h = ((g - b) / delta) % 6;
+        // g es el maximo.
+        else if (cmax == g)
+            h = (b - r) / delta + 2;
+        // b es el maximo.
+        else
+            h = (r - g) / delta + 4;
+    
+        // Math.round redondea el valor que recibe como argumento al numero entero mas cercano.
+        h = Math.round(h * 60);
+    
+        // Si el hue resultara negativo, se transforma a positivo sumandole 360°.
+        if (h < 0)
+            h += 360;
+    
+        // Calcular la luminosidad (lightness). Se calcula primero porque la saturacion depende de la luminosidad.
+        l = (cmax + cmin) / 2;
+    
+        // Calcular la saturacion (saturation).
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+        // Se multiplican s y l por 100 y  se convierten a un string que representa un valor de coma fija mediante la funcion toFixed.
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        // Se retorna un string que representa los 3 componentes del HSV.
+        //return "hsl(" + h + "," + s + "%," + l + "%)";
+        // Se crea un pequeño objeto para encapsular los 3 componentes del hsv y retornarlos.
+        let hsl = {
+            'h': h,
+            's': s,
+            'l': l
+        }
+        return hsl;
+    }
+    
+    function hslToRgb(h, s, l) {
+        // Must be fractions of 1
+        s /= 100;
+        l /= 100;
+        let c = (1 - Math.abs(2 * l - 1)) * s;
+        let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+        let m = l - c / 2;
+        let r = 0;
+        let g = 0;
+        let b = 0;
+    
+        // El hue determina que valores deben tener los componentes r, g y b dependiendo en que rango se encuentra.
+        // Para esto se toma un giro (en 360 grados) y se divide en 6 sectores con un angulo de 60°. 
+        // h esta entre 0 y 60°.
+        if (0 <= h && h < 60) {
+            r = c;
+            g = x;
+            b = 0;
+            // h esta entre 60° y 120°. 
+        } else if (60 <= h && h < 120) {
+            r = x;
+            g = c;
+            b = 0;
+            // h esta entre 120° y 180°. 
+        } else if (120 <= h && h < 180) {
+            r = 0;
+            g = c;
+            b = x;
+            // h esta entre 180° y 240°. 
+        } else if (180 <= h && h < 240) {
+            r = 0;
+            g = x;
+            b = c;
+            // h esta entre 240° y 300°.
+        } else if (240 <= h && h < 300) {
+            r = x;
+            g = 0;
+            b = c;
+            // h esta entre 300° y 360°.
+        } else if (300 <= h && h < 360) {
+            r = c;
+            g = 0;
+            b = x;
+        }
+        // Redondea los valores de r, g y b al numero entero mas cercano.
+        r = Math.round((r + m) * 255);
+        g = Math.round((g + m) * 255);
+        b = Math.round((b + m) * 255);
+        // Se crea un pequeño objeto para encapsular los 3 componentes del rgb y retornarlos.
+        let rgb = {
+            'r': r,
+            'g': g,
+            'b': b
+        };
+        return rgb;
+    }
+    
+    // Se encarga de capturar los 3 input para modificar la matiz, la saturacion y el brillo de la imagen 
+    function adjustHSL(imageData, x, y) {
+        let hRange = parseInt(document.querySelector("#h-range").value, 10);
+        let sRange = parseInt(document.querySelector("#s-range").value, 10);
+        let lRange = parseInt(document.querySelector("#l-range").value, 10);
+    
+        let i = getIndex(imageData, x, y);
+        let rgb = getRGB(imageData, i);
+        let hsl = rgbToHsl(rgb.r, rgb.g, rgb.b);
+        hsl.h += hRange;
+        hsl.s += sRange;
+        hsl.l += lRange;
+    
+        // Ajuste final en caso de que los componentes del HSV se salgan de los rangos permitidos
+        if (hsl.h < 0) {
+            hsl.h = 0;
+        }
+        if (hsl.h > 360) {
+            hsl.h = 360;
+        }
+        if (hsl.s < 0) {
+            hsl.s = 0;
+        }
+        if (hsl.s > 100) {
+            hsl.s = 100;
+        }
+        if (hsl.l > 100) {
+            hsl.l = 100;
+        }
+        
+        rgb = hslToRgb(hsl.h, hsl.s, hsl.l);
+        setPixel(imageData, i, rgb);
     }
 
     // Funcion para descargar el canvas como una imagen
